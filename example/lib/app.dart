7c64eb229fbed696247103251b726905394d9e8e
Ajin Asokan <ajin.panthayil@gmail.com>
2023-03-27 15:06:55 +0530

    upgrade: project files

--- example/lib/app.dart
+++ example/lib/app.dart
@@ -69,62 +69,77 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        // Column is also a layout widget. It takes a list of children and
+        // arranges them vertically. By default, it sizes itself to fit its
+        // children horizontally, and tries to be as tall as its parent.
+        //
+        // Invoke "debug painting" (press "p" in the console, choose the
+        // "Toggle Debug Paint" action from the Flutter Inspector in Android
+        // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+        // to see the wireframe for each widget.
+        //
+        // Column has various properties to control how it sizes itself and
+        // how it positions its children. Here we use mainAxisAlignment to
+        // center the children vertically; the main axis here is the vertical
+        // axis because Columns are vertical (the cross axis would be
+        // horizontal).
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'You have pushed the button this many times:',
+          ),
+          Text(
+            '$_counter',
+            style: Theme.of(context).textTheme.headline4,
+            key: Key("txtCount"),
+          ),
+          Text(
+            'Hello World!',
+            style: Theme.of(context).textTheme.headline4,
+            key: Key("txtGreet"),
+          ),
+          Text.rich(
+            TextSpan(
+              children: [
+                TextSpan(text: "hello"),
+                TextSpan(text: "world"),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-              key: Key("txtCount"),
-            ),
-            Text.rich(
-              TextSpan(
-                children: [
-                  TextSpan(text: "hello"),
-                  TextSpan(text: "world"),
-                ],
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                _counter = 0;
-                setState(() {});
+          ),
+          TextButton(
+            onPressed: () {
+              _counter = 0;
+              setState(() {});
+            },
+            child: Text("Reset"),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: (val) {
+                print("changed $val");
               },
-              child: Text("Reset"),
+              onSubmitted: (val) {
+                print("submitted $val");
+              },
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                onChanged: (val) {
-                  print("changed $val");
-                },
-                onSubmitted: (val) {
-                  print("submitted $val");
-                },
-              ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              key: Key("number_list"),
+              itemBuilder: (ctx, i) {
+                return ListTile(
+                  key: Key("list_item_$i"),
+                  onTap: () {
+                    print("tap on $i");
+                  },
+                  title: Text("$i"),
+                );
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         key: Key("fabIncrement"),
